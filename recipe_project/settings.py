112478e8136ffc41d4f272899e30632c81667fd6
Sasho Stojkoski <112008646+Salewitcher@users.noreply.github.com>
2024-06-29 16:00:43 +0100

    Create project and blog app(have problems with allowed hosts,  will try to solve it later

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j!bihixe82e+to&9ufg=zb#)9t)@1mf!zl0j0yvu6$%hat818r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from recipe_blog import views as recipe_blog_views
 
 urlpatterns = [
+    path("recipe_blog/", recipe_blog_views.my_recipe_blog, name='recipe_blog'),
     path('admin/', admin.site.urls),
 ]
