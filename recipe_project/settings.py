e5d5284803449b3eaa4afd9b4eaa66be238f7459
Sasho Stojkoski <112008646+Salewitcher@users.noreply.github.com>
2024-07-20 18:18:36 +0100

    Create a sign up form

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -148,3 +148,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_FORMS = {
+    'signup': 'users.forms.CustomSignupForm',
+}
--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,10 @@
 
-from django.core.mail import send_mail
+from django import forms
 from allauth.account.forms import SignupForm
 
 class CustomSignupForm(SignupForm):
     def signup(self, request, user):
-        # Send a custom welcome email
-        send_mail(
-            'Welcome to Our Site!',
-            'Hi {},\n\nThank you for signing up at our site! We are excited to have you with us.'.format(user.username),
-            'sashestojkoski@gmail.com',
-            [user.email],
-            fail_silently=False,
-        )
+        # Save additional profile information if provided
+        user.save()
         return user
+
