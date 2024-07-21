5dd26f0c03a26ea6732cd333aeb3dd605b68399e
Sasho Stojkoski <112008646+Salewitcher@users.noreply.github.com>
2024-07-21 18:48:55 +0100

    Update urls.py in the recipes app to include the delete view

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('create/', views.create_recipe, name='create_recipe'),
     path('<int:id>/', views.recipe_detail, name='recipe_detail'),
     path('edit/<int:id>/', views.edit_recipe, name='edit_recipe'),
+    path('delete/<int:id>/', views.delete_recipe, name='delete_recipe'),
     path('', views.recipe_list, name='recipe_list'),
 ]
