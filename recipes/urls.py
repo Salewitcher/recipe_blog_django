a08d6d82cb2357e17726ed1c8596150e599d6f63
Sasho Stojkoski <112008646+Salewitcher@users.noreply.github.com>
2024-07-21 18:24:35 +0100

    Update urls.py in the recipes app to include the detail view

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_recipe, name='create_recipe'),
+    path('<int:id>/', views.recipe_detail, name='recipe_detail'),
+    path('edit/<int:id>/', views.edit_recipe, name='edit_recipe'),
     path('', views.recipe_list, name='recipe_list'),
 ]
