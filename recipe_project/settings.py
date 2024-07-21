a55b23660317597f485bf839ed1c5faea167a045
Sasho Stojkoski <112008646+Salewitcher@users.noreply.github.com>
2024-07-21 13:03:34 +0100

    Create "Forgot Password" for password reset

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -214,6 +214,8 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_USERNAME_REQUIRED = False
 
 
+# EMAIL_BACKEND FOR DEVELOPMENT OR PRODUCTION?
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.your-email-provider.com'
 EMAIL_PORT = 587
