1c4e24a296df9530d86d060b998da6799cb84c97
Sasho Stojkoski <112008646+Salewitcher@users.noreply.github.com>
2024-08-10 11:33:36 +0100

    Update Recipe model to include images

--- recipes/models.py
+++ recipes/models.py
@@ -7,6 +7,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200)
     ingredients = models.TextField()
     instructions = models.TextField()
+    image = models.ImageField(upload_to='recipes/', null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
